27993276075565133bc764a810ca445e933d87ef
MaksimMazurenka <55020608+MaksimMazurenka@users.noreply.github.com>
2022-06-16 22:00:05 +0300

    Add files via upload

---
+++ server/NetServer/NetServer/Controllers/UploadController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetServer.Handler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        private readonly IImageHandler _imageHandler;
+        public UploadController(IImageHandler imageHandler)
+        {
+            _imageHandler = imageHandler;
+        }
+        [HttpPost]
+        public async Task<IActionResult> UploadImage(IFormFile file)
+        {
+            return await _imageHandler.UploadImage(file);
+        }
+    }
+}
---
+++ server/NetServer/NetServer/Utils/ImageWriter.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using NetServer.Interface;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetServer.Utils
+{
+    public class ImageWriter : IImageWriter
+    {
+        public async Task<string> UploadImage(IFormFile file)
+        {
+            if (CheckIfImageFile(file))
+            {
+                return await WriteFile(file);
+            }
+            return "inwalid image file";
+        }
+
+        private async Task<string> WriteFile(IFormFile file)
+        {
+            string fileName;
+            try
+            {
+                var extension = new StringBuilder(".").Append(file.FileName.Split(".")[file.FileName.Split('.').Length - 1]);
+                fileName = new StringBuilder(Guid.NewGuid().ToString()).Append(extension).ToString();
+                string path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot\\images", fileName);
+                using (var bits = new FileStream(path, FileMode.Create))
+                    await file.CopyToAsync(bits);
+            }catch(Exception e)
+            {
+                return e.Message;
+            }
+            return fileName;
+        }
+
+        private bool CheckIfImageFile(IFormFile file)
+        {
+            byte[] fileBytes;
+            using(var ms=new MemoryStream())
+            {
+                file.CopyTo(ms);
+                fileBytes = ms.ToArray();
+            }
+            return WriteHelper.GetImageFormat(fileBytes) != WriteHelper.ImageFormat.unknown;
+        }
+    }
+}
